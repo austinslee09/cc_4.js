ba0212eb9196b395abe1bee88313feda1e4b9b7f
Austin Lee <austinlee@usf.edu>
2025-09-16 09:07:42 -0400

    switch at 1

--- cc_4.js
+++ cc_4.js
@@ -1,8 +1,26 @@
 const products=[
-    {name:"glue", catagory:"crafts", Price:"$4.99", Inventory:"90"},
-    {name:"tires", catagory:"cars", Price:"$60.00", Inventory:"20"},
-    {name:"laptop", catagory:"electronics", Price:"$849.99", Inventory:"40"},
-    {name:"wood", catagory:"materials", Price:"$16.99", Inventory:"70"},
-    {name:"soup", catagory:"food", Price:"$3.99", Inventory:"100"},
+    {name:"glue", catagory:"crafts", Price:["$4.99"],Inventory:"90"},
+    {name:"tires", catagory:"cars", Price:["60.00"], Inventory:"25"},                       
+    {name:"laptop", catagory:"electronics", Price:["$849.99"], Inventory:"40"},
+    {name:"wood", catagory:"materials", Price:["$16.99"], Inventory:"70"},
+    {name:"soup", catagory:"food", Price:["$3.99"], Inventory:"100"}
 ]
 console.log(products)
+
+switch (Price){
+    case 'crafts':
+        discount="20% off"
+    break;
+    case 'cars':
+        discount="15% off"
+    break;
+    case 'electronics':
+    case 'materials':   
+        discount="10% off"
+    break;
+    default:
+        discount="no discount":
+
+}
+
+
