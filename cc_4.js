e9cbf711ca7974e48243bdf3c558fcf5ce9d93c2
Austin Lee <austinlee@usf.edu>
2025-09-16 09:13:23 -0400

    switch at2

--- cc_4.js
+++ cc_4.js
@@ -1,13 +1,13 @@
 const products=[
-    {name:"glue", catagory:"crafts", Price:["$4.99"],Inventory:"90"},
-    {name:"tires", catagory:"cars", Price:["60.00"], Inventory:"25"},                       
-    {name:"laptop", catagory:"electronics", Price:["$849.99"], Inventory:"40"},
-    {name:"wood", catagory:"materials", Price:["$16.99"], Inventory:"70"},
-    {name:"soup", catagory:"food", Price:["$3.99"], Inventory:"100"}
+    {name:"glue", catagory:["crafts"], Price:["$4.99"],Inventory:"90"},
+    {name:"tires", catagory:["cars"], Price:["60.00"], Inventory:"25"},                       
+    {name:"laptop", catagory:["electronics"], Price:["$849.99"], Inventory:"40"},
+    {name:"wood", catagory:["materials"], Price:["$16.99"], Inventory:"70"},
+    {name:"soup", catagory:["food"], Price:["$3.99"], Inventory:"100"}
 ]
 console.log(products)
 
-switch (Price){
+switch (products.catagory){
     case 'crafts':
         discount="20% off"
     break;
@@ -21,5 +21,7 @@ switch (Price){
     default:
         discount="no discount"
 }
+
+
 
 
